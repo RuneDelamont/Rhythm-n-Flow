f150f2e29d7b8ae69d4835e71189316839b634ef
Brian Moore <runedelamont@gmail.com>
2022-07-02 11:31:07 -0700

    Phase 0

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -4,20 +4,13 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-
-//error
-router.get('/hello/world', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
-
-// router.get("/api/csrf/restore", (req, res) => {
-//     const csrfToken = req.csrfToken();
-//     res.cookie("XSRF-TOKEN", csrfToken);
-//     res.status(200).json({
-//       'XSRF-Token': csrfToken
-//     });
-//   });
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
+  });
 
 
 module.exports = router;
