04cec1967169b7ae91b9bba160f274e74b7c2820
Brian Moore <runedelamont@gmail.com>
2022-08-25 18:14:24 -0700

    removed api from root route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const apiRouter = require('./api');
 
 
-router.use('/api', apiRouter);
+router.use('/', apiRouter);
 
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
